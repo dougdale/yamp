150ba756e0a7821145acbb2efd5798d538bceb34
Doug Dale <dougadale@gmail.com>
2017-01-28 18:33:46 -0700

    Add test_yamp_add_passage() unit test

--- test/test_yamp.py
+++ test/test_yamp.py
@@ -5,3 +5,9 @@ class YampTest(unittest.TestCase):
     def test_yamp_init(self):
         y = yamp.Yamp()
         self.assertEqual([], y.passages)
+
+    def test_yamp_add_passage(self):
+        y = yamp.Yamp()
+        passage_text = 'Romans 6:1 What shall we say then? Are we to continue in sin that grace may abound?'
+        y.add_passage(passage_text)
+        self.assertEqual(passage_text, str(y.passages[0]))
